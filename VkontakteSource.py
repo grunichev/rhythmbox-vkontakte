ce7f6c5966767d28b8fe4e262f5a11f795165fe9
Alexey Grunichev <grunichev@gmail.com>
2010-05-06 12:25:47 +0400

    html special entities are allowed in track names (quotes, ampersands etc)

--- VkontakteSource.py
+++ VkontakteSource.py
@@ -24,14 +24,14 @@ def cell_text_data_func(column, cell, model, iterr, db):
 	entry = model.get_value(iterr, 0)
 	markup = ""
 	if db.entry_get(entry, rhythmdb.PROP_TITLE):
-		markup += "<b>{0}</b>".format(glib.markup_escape_text(db.entry_get(entry, rhythmdb.PROP_TITLE)))
+		markup += "<b>{0}</b>".format(db.entry_get(entry, rhythmdb.PROP_TITLE).replace('<', '&lt;').replace('>', '&gt;'))
 	if db.entry_get(entry, rhythmdb.PROP_DURATION):
 		seconds = db.entry_get(entry, rhythmdb.PROP_DURATION)
 		minutes = seconds / 60
 		seconds = seconds % 60
 		markup += " - {0}:{1}\n".format(minutes, format(seconds, "02"))
 	if db.entry_get(entry, rhythmdb.PROP_GENRE):
-		markup += glib.markup_escape_text(db.entry_get(entry, rhythmdb.PROP_GENRE))
+		markup += db.entry_get(entry, rhythmdb.PROP_GENRE).replace('<', '&lt;').replace('>', '&gt;')
 	cell.props.markup = markup
 	cell.props.wrap_width = column.get_width()
 	cell.props.wrap_mode = pango.WRAP_WORD
